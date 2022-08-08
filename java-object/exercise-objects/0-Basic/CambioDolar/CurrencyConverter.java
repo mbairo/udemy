0564d74c8eb83fac59c82ddfbe0d9821ad6a4821
mbairo <bairo.marcos@gmail.com>
2022-08-08 15:39:15 -0300

    Delete java-object directory

--- java-object/exercise-objects/0-Basic/CambioDolar/CurrencyConverter.java
+++
@@ -1,5 +0,0 @@
-public class CurrencyConverter{
-    public static double cost(double dollarPrice, double purchase){
-        return dollarPrice * purchase * 1.06;  // 1.06 = 6% de IOF/Imposto
-    }
-}
